6fddc269d2df63a66382e1379e33edaaad4a7641
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-03-25 15:13:46 -0300

    configurado inputs e component Table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from './Table/Table';
 
 class App extends Component {
     state = {
@@ -7,13 +9,61 @@ class App extends Component {
             t_ultima_chegada:         {name: 'tempo desde a última chegada (minutos) ',         value: 0},
             t_chegada_relogio:        {name: 'Tempo de Chegada no Relógio',        value: 0},
             t_servico:                {name: 'Tempo de Serviço (minutos)',                value: 0},
-            t_inicio_servico_relogio: {name: 'Tempo de ìnício do Serviço no Relógio', value: 0},
+            t_inicio_servico_relogio: {name: 'Tempo de início do Serviço no Relógio', value: 0},
             t_cliente_fila:           {name: 'Tempo do Cliente na Fila (minutos)',           value: 0},
             t_final_servico_relogio:  {name: 'Tempo Final do Serviço no Relógio',  value: 0},
             t_cliente_no_sistema:     {name: 'Tempo do cliente no Sistema (minutos)',     value: 0},
             t_livre_operador:         {name: 'Tempo Livre do Operador (minutos)',         value: 0}
-        }        
+        },
+        tmp_alt_chegada: [],
+        tmp_alt_servico: [],
+        total_time: 0,
+        html_table_tbody: ''
     };
+
+    saveTimeHandler = (type) => {
+        if (type === 'chegada') {
+            let arr = this.state.tmp_alt_chegada;
+            arr.push(this.refs.lastStart.value);
+            this.refs.lastStart.value = "";
+
+            this.setState({
+                tmp_alt_chegada: arr
+            });
+            this.refs.textAreaAddedValues.value = arr;
+
+        } else if (type === 'servico') {
+            let arr = this.state.tmp_alt_servico;
+            arr.push(this.refs.service.value);
+            this.refs.service.value = "";
+
+            this.setState({
+                tmp_alt_servico: arr
+            });
+            this.refs.addedValuesService.value = arr;
+
+        } else if (type === 'total_time') {
+            let value = this.refs.total_time.value; 
+            this.setState({
+                total_time: value
+            });
+
+            this.refs.total_time.value = "";
+            this.refs.addedValuesTotalTime.value = value;
+        }
+    }
+
+    
+    constructTable = () => {
+
+        let htmlContent = (
+            Object.keys(this.state.tempos).map((item, index) => (
+                <th key={index}> {this.state.tempos[item].name} </th>
+            ))
+        );
+    }
+
+
 
     render() {
         let th = null;
@@ -21,27 +71,50 @@ class App extends Component {
             Object.keys(this.state.tempos).map((item, index) => (
                 <th key={index}> {this.state.tempos[item].name} </th>
             ))
-        )
+        )        
 
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Simulação</h1>
                 </header>
-                <table>
+                <div>
+                    <div>
+                        <label htmlFor="lastStart">Tempo desde a última chegada</label>
+                        <input type="text" ref="lastStart" name="lastStart"/>
+                        <button onClick={ () => this.saveTimeHandler('chegada')} >add</button>
+                        <br/>
+                        <br/>
+                        <textarea name="addedValues" cols="20" rows="4" ref="textAreaAddedValues" ></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="service">Tempo de serviço</label>
+                        <input type="text" ref="service" name="service"/>
+                        <button onClick={ () => this.saveTimeHandler('servico') } >add</button>
+                        <br/>
+                        <br/>
+                        <textarea name="addedValues" cols="20" rows="4" ref="addedValuesService" ></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="total_time">Tempo total da simulação</label>
+                        <input type="text" ref="total_time" name="total_time"/>
+                        <button onClick={ () => this.saveTimeHandler('total_time') } >add</button>
+                        <br/>
+                        <br/>
+                        <textarea name="addedValues" cols="10" rows="1" ref="addedValuesTotalTime" ></textarea>
+                    </div>
+                </div>
+                <button onClick={this.constructTable} >Simular</button>
+                <table >
                     <thead>
                         <tr>
-                            {th}
+                            <th>Cliente</th>
                         </tr>
+                        {th}
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                teste
-                            </td>
-                            <td>
-                                teste
-                            </td>
+                        <tr ref="line">
+                            <Table tmp_alt={this.state.tmp_alt_chegada.length} />
                         </tr>
                     </tbody>
                 </table>
@@ -51,3 +124,4 @@ class App extends Component {
 }
 
 export default App;
+
---
+++ src/Table/Table.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Table = (props) => {
+
+    console.log(props);
+
+    // let total_generated = 0;
+    // let size = this.state.tmp_alt_chegada.size;
+    // console.log("size", size);
+    // while (total_generated <= this.state.total_time ) {
+    //     total_generated += this.state.tmp_alt_chegada
+    // }
+
+    return (
+       <td>text</td>     
+    );
+}
+
+export default Table;
