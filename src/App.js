fe004d5cabb5d56f9dd5e29ae1fc80c7425cdeb0
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-03-27 21:08:18 -0300

    buil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Table} from 'react-bootstrap'
 
 class App extends Component {
     state = {
